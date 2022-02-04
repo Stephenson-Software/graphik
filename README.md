90e1ddb1f7005f67dcede040cea03b21218efb72
dmccoystephenson <dmccoystephenson@gmail.com>
2022-02-03 18:00:49 -0700

    Migrated to python and utilized pygame for some methods.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GrafikLib
-This library is intended to assist developers with graphics programming. The project utilizes SDLAssist to interface with SDL.
+This library is intended to assist developers with graphics programming.
+
+## Dependencies
+- pygame
---
+++ src/Graphik.py
@@ -0,0 +1,34 @@
+import pygame
+
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0,200,0)
+blue = (0,0,200)
+
+#  @author Daniel McCoy Stephenson
+#  @since February 3rd, 2022
+class Graphik:
+    def __init__(self, gameDisplay):
+        self.gameDisplay = gameDisplay
+
+    def drawRectangle(self, xpos, ypos, width, height, color):
+        pygame.draw.rect(self.gameDisplay, color, [xpos, ypos, width, height])
+
+    def drawText(self, text, xpos, ypos, size, color):
+        myFont = pygame.font.Font('freesansbold.ttf', size)
+        textSurface = myFont.render(text, True, color)
+        textRectangle = textSurface.get_rect()
+        textRectangle.center = ((xpos, ypos))
+        self.gameDisplay.blit(textSurface, textRectangle)
+
+    def drawButton(self, xpos, ypos, width, height, colorBox, colorText, sizeText, text, function):
+        self.drawRectangle(xpos, ypos, width, height, colorBox)
+        self.drawText(text, xpos + (width//2), ypos + (height//2), sizeText, white)
+        
+        # if clicked then do function
+        mouse = pygame.mouse.get_pos()
+        if (xpos + width > mouse[0] > xpos and ypos + height > mouse[1] > ypos):
+            click = pygame.mouse.get_pressed()
+            if click[0] == 1:
+                function()
